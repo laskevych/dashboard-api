a61261a8799f4d679bd295630de1d0e5eecc1674
Andrew Laskevych <an.laskevych@gmail.com>
2022-08-28 20:26:35 +0300

    Add middleware

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const port = 8000;
 const app = express();
 
 app.get('/hello', (req, res) => {
-    res.send('hello');
+    throw new Error('Fatal error!');
 });
 
 app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(500).send(`Server error! Details: ${err?.message}`);
+});
 
 app.listen(port, () => {
     console.log(`Server are working on http://localhost:${port}/`);
