5146578bcba6131e2c5d782d6c5e8c5a1b22c863
Andrew Laskevych <an.laskevych@gmail.com>
2022-08-28 20:47:35 +0300

    Update App for TypeScript

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import { userRouter } from './users/users.js';
 
@@ -11,7 +11,7 @@ app.get('/hello', (req, res) => {
 
 app.use('/users', userRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err.message);
     res.status(500).send(`Server error! Details: ${err?.message}`);
 });
