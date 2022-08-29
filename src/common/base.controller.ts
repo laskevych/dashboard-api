95b9ceb9bb89d6e97f1328811a8175f7b96de886
Andrew Laskevych <an.laskevych@gmail.com>
2022-08-29 10:56:22 +0300

    Add base controller

---
+++ src/common/base.controller.ts
@@ -0,0 +1,36 @@
+import { LoggerService } from "../logger/logger.service";
+import { Router, Response } from "express";
+import { IControllerRoute } from "./route.interface";
+
+export abstract class BaseController {
+    private logger: LoggerService;
+    private readonly _router: Router;
+
+    constructor(logger: LoggerService) {
+        this.logger = logger;
+        this._router = Router();
+    }
+
+     get router() {
+        return this._router;
+     }
+
+     public send<T>(res: Response, code: number, message: T) {
+        res.type('application/json');
+        return res.status(code).json(message);
+     }
+
+     public ok<T>(res: Response, message: T) {
+        return this.send<T>(res, 200, message);
+     }
+
+     protected bindRoutes(routes: IControllerRoute[]) {
+        for (const route of routes) {
+            this.logger.log(`Init router. Method: ${route.method.toUpperCase}; Path: ${route.path.toLocaleLowerCase}`)
+
+            const handler = route.func.bind(this);
+
+            this._router[route.method](route.path, handler);
+        }
+     }
+}
